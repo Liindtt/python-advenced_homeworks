80a7b42a4078a77bb5e0de02eaae5b740504bc07
Lindtt <lindtt.10.2003@gmail.com>
2024-05-22 15:38:39 +0300

    update homework 25

--- 25 lesson/main.py
+++ 25 lesson/main.py
@@ -1,19 +1,22 @@
 import asyncio
+from time import perf_counter
 
 
 async def add_one(limit):
     num = 0
     while num < limit:
-        num += 1
         print(f"{num=}")
+        num += 1
         await asyncio.sleep(1)
 
 
 async def main():
+    start_time = perf_counter()
     first = asyncio.create_task(add_one(12))
     second = asyncio.create_task(add_one(8))
     await first
     await second
+    print(f"Program ends in: {perf_counter() - start_time:.2f} seconds")
 
 
 asyncio.run(main())
